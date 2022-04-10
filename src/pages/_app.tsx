d34f28bac04785b14202376bbf28c10c1bb062f4
hiro0214 <leonhurt2@gmail.com>
2022-04-11 02:12:23 +0900

    add: types-scss-modules

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { useEffect } from 'react';
 import { commonModules } from '../modules/common/index';
-import '../style/style.scss';
+import '../styles/style.scss';
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   useEffect(() => {
